2f458f3b4185b0367055ad8de28eb6210d8a79fc
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-04 23:46:26 +0200

    add-text-reading-pdf-fils-add-pypdf2

---
+++ code3.py
@@ -0,0 +1,21 @@
+# python read pdf file
+# importing required modules
+import PyPDF2
+
+# creating a pdf file object
+pdfFileObj = open('modale.pdf', 'rb')
+
+# creating a pdf reader object
+pdfReader = PyPDF2.PdfReader(pdfFileObj)
+
+# printing number of pages in pdf file
+print(len(pdfReader.pages))
+'''
+# creating a page object
+pageObj = pdfReader.Pages[0]
+
+# extracting text from page
+print(pageObj.extract_text())
+'''
+# closing the pdf file object
+pdfFileObj.close()
