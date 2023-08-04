8ed870c0e396bd35077f67c1582667f5b1193abc
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-04 20:41:07 +0200

    add-text-reading-adding-data

--- text-reading-code.py
+++ text-reading-code.py
@@ -1,6 +1,10 @@
 
-f = open('text.txt','r')
-data = f.readlines()
+#f = open('text.txt','r')
+#data = f.readlines()
 #print(data)
-for n in data:
-    print(n) 
+#for n in data:
+    #print(n)
+
+f = open('text2.txt','a') #python append to file
+f.write('\n mystro')
+f.close()
