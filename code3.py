03e66fe6d91a6e1987eb88514abc79abc87bbd21
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-05 11:46:31 +0200

    add-anas1.txt-with 'a'context manager

--- code3.py
+++ code3.py
@@ -3,7 +3,11 @@ f = open('Anas.txt','a') #python append to file
 f.write('\n Halllo Anas')
 f.close()
 '''
-
+'''
 f = open('Anas1.txt','w') #python append to file
 f.write('\n Welcome Anas')
 f.close()
+'''
+
+with open('Mystro.txt','a')as file:
+    file.write('Test ..........')
