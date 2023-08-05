594a108bf46b66200ee22d44046df29d697d0154
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-05 11:40:14 +0200

    add-anas1.txt-with 'a' or 'w'

--- code3.py
+++ code3.py
@@ -1,3 +1,9 @@
+'''
 f = open('Anas.txt','a') #python append to file
 f.write('\n Halllo Anas')
+f.close()
+'''
+
+f = open('Anas1.txt','w') #python append to file
+f.write('\n Welcome Anas')
 f.close()
